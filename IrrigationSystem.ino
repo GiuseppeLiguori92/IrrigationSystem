bcb9c4d289d1284f2d5b2551400d98d37233f9a1
Giuseppe Liguori <giuseppe.liguori92@hotmail.it>
2018-06-05 22:58:18 +0100

    add water sensor

--- IrrigationSystem.ino
+++ IrrigationSystem.ino
@@ -2,27 +2,70 @@
 
 // Version 1.0
 
+// Digital
 const int PUMP = 13;
-const int POWER = 2;
+const int BUZZER = 12;
 
+// Analog
+const int WATER_SENSOR = 0;
+
+// Serial 
 const int SERIAL_PORT = 9600;
 
-const int LOOP_FREQUENCY_MS = 1000;
+// Delay
+const int DELAY = 1000;
+
+// Variables
+const int MIN_WATER_LEVEL = 0;
+
+bool buzzer = false;
+bool pump = false;
 
 void setup() {
   Serial.begin(SERIAL_PORT);
+  
   // Input pins
-  pinMode(POWER, INPUT);
+  pinMode(WATER_SENSOR, INPUT);
+  
   // Output pins
   pinMode(PUMP, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
 }
 
 void loop() {
-  bool power = digitalRead(POWER);
-  
-  digitalWrite(PUMP, power);
+  waterSensorManager();
 
-  Serial.println(power);
+  pumpManager();
   
-  delay(LOOP_FREQUENCY_MS);
+  delay(DELAY);
 }
+
+void pumpManager() {
+  digitalWrite(PUMP, pump);
+}
+
+void waterSensorManager() {
+  int sensorLevel = analogRead(WATER_SENSOR);
+  Serial.print("Water: "); Serial.println(sensorLevel);
+
+  if (sensorLevel < MIN_WATER_LEVEL) {
+      alertUserAboutWaterLevelWithBuzzer();
+  } else {
+      resetBuzzer();
+  }
+}
+
+void alertUserAboutWaterLevelWithBuzzer() {
+  buzzer = !buzzer;
+  updateBuzzer();
+}
+
+void resetBuzzer() {
+  buzzer = LOW;
+  updateBuzzer();
+}
+
+void updateBuzzer() {
+  digitalWrite(BUZZER, buzzer);
+}
+
